7d40da487f3316d845109bc56810449e78da0455
Carl Olson <colson9@student.cscc.edu>
2020-08-08 23:47:49 -0400

    spinner overlay added to indicate activity

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux'
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -16,6 +16,18 @@ class LoginForm extends Component {
   onButtonPress() {
     const { email, password } = this.props;
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner message='Loading...'/>;
+    }
+    return (
+      <Button afterPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
+
   }
 
   renderError() {
@@ -57,21 +69,16 @@ class LoginForm extends Component {
           {this.renderError()}
         </View>
         <CardSection>
-          <Button afterPress={this.onButtonPress.bind(this)}>
-            Login
-            </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password,
-    error: state.auth.error
-  };
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading } = auth;
+  return { email, password, error, loading };
 };
 
 const styles = {
