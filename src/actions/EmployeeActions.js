838ad4a3b5ea1ec1e743fe1cf47f0afb5908d700
Carl Olson <colson9@student.cscc.edu>
2020-08-22 16:22:35 -0400

    deprecated ListItem replaced with FlatList

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -13,6 +16,23 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
   console.log(name, phone, shift);
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.employeeList({ type: 'reset' })
+      });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
+  };
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,17 +1,86 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import firebase from 'firebase';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SafeAreaView, StyleSheet, FlatList, View, Text } from 'react-native';
+import ListItem from './ListItem';
+import {CardSection} from '../components/common';
+
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
-  render() {
+  componentDidMount() {
+    this.props.employeesFetch();
+  }
+
+  createDataSource(employees) {
+    let employeeArr = [];
+    if (employees) {
+      employeeArr = Object.keys(employees).map(function (k, y) {
+        let empObj = employees[k]
+        empObj.key = k;
+        return empObj;
+      })
+    }
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
-    );
-  } 
+      <FlatList
+        data={employeeArr}
+        renderItem={({item}) => <ListItem item={item}></ListItem>}
+        //renderItem={({ item }) => <CardSection><Text style={styles.item}>{item.name}</Text></CardSection>}
+      />
+    )
+  }
+
+  render() {
+    if (this.props.employees) {
+      return (
+        <View>
+          {this.createDataSource(this.props.employees)}
+        </View>
+      )
+    }
+  }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = state.employees
+  return { employees };
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    paddingLeft: 20,
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+
+// const mapStateToProps = state => {
+//   const employees = _.map(state.employees, (val, uid) => {
+//     return { ...val, uid };
+//   });
+//   return { employees };
+// };
+
+// const styles = StyleSheet.create({
+//   // container: {
+//   //   flex: 1,
+//   //   marginTop: StatusBar.currentHeight || 0,
+//   //},
+//   item: {
+//     backgroundColor: '#f9c2ff',
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//   },
+// });
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
